df46deee1f4984c667baa570e7e9451f0b8592c9
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-10-04 15:50:14 +0300

    DONE

---
+++ code-challenge/app/migrations/versions/458d753379f3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 458d753379f3
+Revises: efd38cab2a39
+Create Date: 2023-10-04 15:00:32.738474
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '458d753379f3'
+down_revision = 'efd38cab2a39'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
